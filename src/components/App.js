223ce560d417553ce5b1e7cc650e29310117a303
Chloe ONeil <chloesporri@yahoo.com>
2023-06-25 16:36:35 -0700

    Add portfolioControl import to App
    Co-authored-by: nsncowan <nsncowan@users.noreply.github.com>
    Co-authored-by: Geoffrey Gao <geoffreygao1@users.noreply.github.com>
    Co-authored-by: Najma A <anajmabdi@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
+import PortfolioControl from './PortfolioControl';
 
 function App() {
   return (
   <React.Fragment>
     <Header />
+    <PortfolioControl />
     <hr/>
   </React.Fragment>
   );
