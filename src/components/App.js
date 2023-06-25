8e1a9da24021752346a75df233599d6f793fecd2
Chloe ONeil <chloesporri@yahoo.com>
2023-06-25 09:52:08 -0700

    add header to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
-  
-  )
+  <React.Fragment>
+    <Header />
+    <hr/>
+  </React.Fragment>
+  );
 }
 
 export default App;
