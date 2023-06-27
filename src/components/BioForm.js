70e3988ede9b30f1fea05867d779de883c48e546
Chloe ONeil <chloesporri@yahoo.com>
2023-06-26 20:43:32 -0700

    add bioForm.js and boiler

---
+++ src/components/BioForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types"; 
+
+
+function BioForm(props) {
+
+  // form submission function
+  function newBioFormSubmission(event) {
+    event.preventDefault();
+    props.addNewBioProp1({
+      bioName: event.target.bioName.value,
+      bioText: event.target.bioText.value
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <form onSubmit={newBioFormSubmission}>
+        <input 
+          type="text" 
+          name="bioName" 
+          placeholder="Enter your name" />
+        <textarea 
+          name="bioText" 
+          placeholder="Write your bio here" />
+        <button type="submit">{buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+// PropTypes
+BioForm.propTypes = {
+  addNewBioProp1: PropTypes.func
+};
+
+export default BioForm;
